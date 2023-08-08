977694c6e87bce1c71d0bb43631917395c6b9aea
Enzoerb <enzoerbert@gmail.com>
2023-08-08 17:36:58 -0300

    a better interface

--- interface/main.py
+++ interface/main.py
@@ -19,67 +19,89 @@ class WebcamApp:
         # Get the dimensions of the video frame
         self.width = int(self.vid.get(cv2.CAP_PROP_FRAME_WIDTH))
         self.height = int(self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        
+
+        self.aspect_ratio = self.width / self.height
+        self.canvas_width = 800
+        self.canvas_height = int(self.canvas_width / self.aspect_ratio)
+
+
         # Tkinter setup
-        self.canvas = tk.Canvas(window, width=self.width, height=self.height)
-        self.canvas.pack()
+        self.canvas = tk.Canvas(window, width=self.canvas_width, height=self.canvas_height)
+        self.canvas.pack(side=tk.RIGHT)
 
         # Color selection buttons
         colors = {
-            "Cyan": "#00FFFF",
-            "Red": "#FF0000",
-            "Black": "#000000",
-            "Aspargus": "#799f63",
-            "Lilac": "#C8A2C8"
+            "Azul": "#596E7F",
+            "Vermelho": "#660B1C",
+            "Roxo": "#1B0617",
+            "Verde": "#799F63",
+            "Lilas": "#baa3bd"
         }
         
+        # Create color selection buttons with colored squares
         self.color_btns = []
+
+        btn_size = 20
+
         for color_name, hex_code in colors.items():
-            button = tk.Button(window, text=color_name, width=10, command=lambda h=hex_code: self.set_color(h))
-            self.color_btns.append(button)
-            button.pack(side=tk.LEFT, padx=5, pady=10)
-        
-        self.btn_choose_color = tk.Button(window, text="Choose Color", width=12, command=self.choose_color)
-        self.btn_choose_color.pack(side=tk.LEFT, padx=5, pady=10)
-        
-        self.btn_quit = tk.Button(window, text="Quit", width=10, command=self.quit)
-        self.btn_quit.pack(pady=10)
-       
-        self.current_color = "#00FFFF"
- 
+            button_frame = tk.Frame(window)
+
+            color_square = tk.Canvas(button_frame, width=btn_size, height=btn_size, background=hex_code, highlightthickness=0)
+            color_square.pack(side=tk.LEFT)
+
+            button = tk.Button(button_frame, text=color_name, width=int(btn_size/2), command=lambda h=hex_code: self.set_color(h))
+            button.pack(side=tk.LEFT)
+
+            self.color_btns.append(button_frame)
+            button_frame.pack(side=tk.TOP, anchor=tk.W, padx=5, pady=10)
+
+        choose_button_frame = tk.Frame(window)
+        self.choose_color_square = tk.Canvas(choose_button_frame, width=btn_size, height=btn_size, background='white', highlightthickness=0)
+        self.choose_color_square.pack(side=tk.LEFT)
+
+        # Place the "Choose Color" and "Quit" buttons
+        self.btn_choose_color = tk.Button(choose_button_frame, text="Choose Color", width=int(btn_size/2), command=self.choose_color)
+        self.btn_choose_color.pack(side=tk.LEFT)
+        choose_button_frame.pack(side=tk.TOP, anchor=tk.W, padx=5, pady=10)
+
+        self.btn_quit = tk.Button(window, text="Quit", width=int(btn_size/2), command=self.quit)
+        self.btn_quit.pack(side=tk.BOTTOM, anchor=tk.W, padx=5, pady=10)
+
+        self.window.update_idletasks()
+
+        self.current_color = "#799F63"
+
         # After setting up the Tkinter window, start the webcam capture
         self.update()
         self.window.mainloop()
-    
+
+
     def quit(self):
+        print('Quitting...')
         self.window.quit()
+
 
     def set_color(self, hex_code):
         self.current_color = hex_code
 
+
     def choose_color(self):
         color = colorchooser.askcolor(title="Choose Color")
         if color[1] is not None:
-            self.current_color = color[1]
-    
-    def update2(self):
-        ret, frame = self.vid.read()
-        if ret:
-            # Convert the frame from OpenCV's BGR format to RGB format
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            # Convert the frame to a PIL image
-            image = Image.fromarray(frame)
-            # Convert the PIL image to a Tkinter PhotoImage
-            self.photo = ImageTk.PhotoImage(image=image)
-            # Show the new frame on the canvas
-            self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
-        self.window.after(10, self.update)
+            self.set_color(color[1])
+            self.choose_color_square.config(background=color[1])
+        else:
+            self.set_color(None)
+            self.choose_color_square.config(background='white')
+
 
     def update(self):
         ret, frame = self.vid.read()
         if ret:
+
+            resized_frame = cv2.resize(frame, (self.canvas_width, self.canvas_height))
             # Call the process_image function on the webcam frame
-            processed_frame = self.image_processor(frame, self.model, self.current_color)
+            processed_frame = self.image_processor(resized_frame, self.model, self.current_color)
             
             # Convert the frame from OpenCV's BGR format to RGB format
             processed_frame = cv2.cvtColor(processed_frame, cv2.COLOR_BGR2RGB)
@@ -96,9 +118,8 @@ class WebcamApp:
         self.window.after(10, self.update)
 
 if __name__ == "__main__":
-    def mocked_processor(frame):
+    def mocked_processor(frame, model, current_color):
         return frame
 
     root = tk.Tk()
-    app = WebcamApp(root, "Webcam Capture", mocked_processor)
-
+    app = WebcamApp(root, "Webcam Capture", mocked_processor, None)
